fe24060d58882001808e10d6f1c38808e7cf3478
RaihanaPoli <raihana27@gmail.com>
2017-05-22 18:45:28 +0300

    Add files via upload

---
+++ vivado/project_Audio/project_Audio.sdk/Audio_test/Debug/src/helloworld.d
@@ -0,0 +1,18 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../Audio_test_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Audio_test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Audio_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Audio_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../Audio_test_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Audio_test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Audio_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Audio_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
---
+++ vivado/project_Audio/project_Audio.sdk/Audio_test/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../Audio_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Audio_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Audio_test_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../Audio_test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../Audio_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Audio_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Audio_test_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../Audio_test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
